63f8d211aee07d9e174ed0127e0538d38c13c34a
David Ang <davidang09@gmail.com>
2019-08-12 01:11:21 +0800

    start showing transcript and style the layout a bit

--- src/js/App.js
+++ src/js/App.js
@@ -8,7 +8,7 @@ import playGame from './game'
 
 export const config = {
   type: Phaser.AUTO,
-  parent: 'phaser',
+  parent: 'game',
   width: 400,
   height: 600,
   scene: playGame,
@@ -29,7 +29,8 @@ class App extends React.Component {
     loading: true,
     provider: 'None',
     networkType: 'None',
-    balance: 0
+    balance: 0,
+    transactionReceipt: ''
   }
 
   componentDidMount() {
@@ -51,55 +52,15 @@ class App extends React.Component {
 
           const scoreContract = await web3Client.getContractInstance()
           const contract = new web3Client.web3.eth.Contract(scoreContract.abi, scoreContract.address);
-          // const res = await contract.methods.setScore(55).send({ from: accountId})
+          const res = await contract.methods.setScore(55).send({ from: accountId})
           // console.log('setScore', res)
 
-          web3.eth.filter({
-            address: scoreContract.address,
-            from: 0,
-            to: 'latest'
-            }).get(function (err, result) {
-              console.log(err, result)
-          })
+          const web3 = web3Client.web3
+          const receipt = await web3.eth.getTransactionReceipt(res.transactionHash)
+          this.setState({transactionReceipt: JSON.stringify(receipt, null, 2) })
+          // console.log('receipt', receipt)
         }
       })
-
-    // const web3 = web3Client.getWeb3()
-
-    // var myContract = new web3.eth.Contract(contract.abi, contract.address);
-    // console.log(myContract)
-    // const res = await myContract.methods.setScore(55).send({ from: this.state.accountId})
-    // console.log('setScore', res)
-    // const res2 = await myContract.methods.getScore(this.state.accountId).call()
-    // console.log('getScore', res2)
-    // //contract.totalSupply().then(d => console.log(d))
-
-    // console.log(web3.utils.toWei('0.05', 'ether'))
-
-    // web3.eth.sendTransaction({
-    //   from: this.state.accountId,
-    //   to: contract.address,
-    //   value: web3.utils.toWei('0.05', 'ether')
-    // })
-    // .on('transactionHash', function(hash){
-    //   console.log('transactionHash', hash)
-    // })
-    // .on('receipt', function(receipt){
-    //   console.log('receipt', receipt)
-    // })
-    // .on('confirmation', function(confirmationNumber, receipt){
-    //   console.log('confirmationNumber', confirmationNumber, receipt)
-    // })
-    // .on('error', (error, receipt) => { console.log('error', error, receipt) } ) // If a out of gas error, the second parameter is the receipt.
-
-    //contract.sendTransaction()
-    // contract.send(web3.utils.toWei('0.05', 'ether')).then(function(result) {
-    //   console.log('result', result)
-    // }).catch(e  => console.log('Transaction failed', e))
-
-    //  const myContract = getInstance('ScoreToken')
-    //  console.log(myContract)
-
   }
 
   render() {
@@ -108,23 +69,34 @@ class App extends React.Component {
         <header className="App-header">
           <h1 className="App-title">Crypto Stacker</h1>
         </header>
-        <p>React version { React.version }</p>
-        <DetectMetamask></DetectMetamask>
-        Ethereum Blockchain Connection:
-        <BounceLoader
-          css={`display:inline;`}
-          sizeUnit={'rem'}
-          size={1}
-          color={this.state.loading?'red':'green'}
-        />
-        { this.state.isConnected &&
-          <div>
-            <p>Account Id: { this.state.accountId }</p>
-            <p>Provider: { this.state.provider }</p>
-            <p>Network type: { this.state.networkType }</p>
-            <p>Balance: { this.state.balance }</p>
+        <div className="row">
+          <div className="col">
+            <div id="game"></div>
           </div>
-        }
+          <div className="col">
+            <p>React version { React.version }</p>
+            <DetectMetamask></DetectMetamask>
+            Ethereum Blockchain Connection:
+            <BounceLoader
+              css={`display:inline;`}
+              sizeUnit={'rem'}
+              size={1}
+              color={this.state.loading?'red':'green'}
+            />
+            { this.state.isConnected &&
+              <div>
+                <p>Account Id: { this.state.accountId }</p>
+                <p>Provider: { this.state.provider }</p>
+                <p>Network type: { this.state.networkType }</p>
+                <p>Balance: { this.state.balance }</p>
+              </div>
+            }
+            <pre className="receipt">
+              Transaction Receipt:
+              {this.state.transactionReceipt}
+            </pre>
+          </div>
+        </div>
       </div>
     )
   }
